705f88dda0c3a3ed4b6cf289adb011815c0d6f69
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-01-22 17:45:49 -0500

    finished

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+// function createInstructor(firstName, lastName) {
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//   };
+// }
+
 function createInstructor(firstName, lastName) {
+  return { firstName, lastName };
+}
+
+var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!",
+};
+
+instructor[favoriteNumber] = "That is my favorite!";
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+var instructor = {
+  firstName: "Colt",
+  sayHi() {
+    return "Hi!";
+  },
+  sayBye() {
+    return this.firstName + " says bye!";
+  },
+};
+
+function createAnimal(species, verb, noise) {
   return {
-    firstName: firstName,
-    lastName: lastName,
+    species,
+    [verb]() {
+      return noise;
+    },
   };
 }
